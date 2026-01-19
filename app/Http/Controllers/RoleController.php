cb991549a3756cbee351cb9f156d95fce52f3176
Nowshad <lewsedmond@gmail.com>
2026-01-20 01:07:53 +0600

    update the permition

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,19 +14,19 @@ class RoleController extends Controller
      */
     public function index()
     {
-       $roles = Role::all()->map(function ($role) {
-        return [
-            'id' => $role->id,
-            'name' => $role->name,
-            'guard_name' => $role->guard_name,
-            'permissions' => $role->permissions->pluck('name')->toArray(),
-        ];
-       });
+        $roles = Role::all()->map(function ($role) {
+            return [
+                'id' => $role->id,
+                'name' => $role->name,
+                'guard_name' => $role->guard_name,
+                'permissions' => $role->permissions->pluck('name')->toArray(),
+            ];
+        });
 
-    
-       return inertia('role/index', [
-        'roles' => $roles,
-       ]);
+
+        return inertia('role/index', [
+            'roles' => $roles,
+        ]);
     }
 
     /**
@@ -35,8 +35,8 @@ class RoleController extends Controller
     public function create()
     {
         $permitons = Permission::all();
-        return inertia('role/create',[
-            'permissions'=>$permitons
+        return inertia('role/create', [
+            'permissions' => $permitons
         ]);
     }
 
@@ -45,18 +45,18 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-       $request->validate([
-        'name'=>'required|unique:roles,name',
-        'guard_name'=>'required',
-        'permissions'=>'array'
-       ]);
-         $role = Role::create([
-          'name'=>$request->name,
-          'guard_name'=>$request->guard_name
-         ]);
-            if($request->has('permissions')){
-                $role->syncPermissions($request->permissions);
-            }   
+        $request->validate([
+            'name' => 'required|unique:roles,name',
+            'guard_name' => 'required',
+            'permissions' => 'array'
+        ]);
+        $role = Role::create([
+            'name' => $request->name,
+            'guard_name' => $request->guard_name
+        ]);
+        if ($request->has('permissions')) {
+            $role->syncPermissions($request->permissions);
+        }
         return Inertia::location(route('users.permissions'));
     }
 
@@ -73,13 +73,18 @@ class RoleController extends Controller
      */
     public function edit(string $id)
     {
-        $role = Role::findById($id);
+       try {
+            $role = Role::findById($id);
+        } catch (\Exception $e) {
+            $role = Role::findOrFail($id,'api');
+        }
+
         $permissions = Permission::all();
-        $rolePermissions = $role->permissions->pluck('id')->toArray();
-        return inertia('role/edit',[
-            'role'=>$role,
-            'permissions'=>$permissions,
-            'rolePermissions'=>$rolePermissions
+
+        return Inertia::render('role/edit', [
+            'role' => $role,
+            'permissions' => $permissions,
+            'rolePermitssions' => $role->permissions->pluck('id')->toArray(),
         ]);
     }
 
@@ -88,7 +93,32 @@ class RoleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        if ($request->name === 'admin') {
+            return Inertia::location(route('users.permissions'));
+        } else {
+            $role = Role::findById($id);
+
+            $request->validate([
+                'name' => 'required|unique:roles,name,' . $id,
+                'guard_name' => 'required',
+                'permission_ids' => 'array'
+            ]);
+            $role->update([
+                'name' => $request->name,
+                'guard_name' => "web"
+            ]);
+
+            if ($request->filled('permission_ids')) {
+                $permissions = Permission::whereIn('id', $request->permission_ids)
+                    ->where('guard_name', $request->guard_name)
+                    ->get();
+
+                $role->syncPermissions($permissions);
+            } else {
+                $role->syncPermissions([]);
+            }
+            return Inertia::location(route('users.permissions'));
+        }
     }
 
     /**
--- resources/js/pages/role/edit.tsx
+++ resources/js/pages/role/edit.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
-import { BreadcrumbItem,  } from '@/types';
-import { Head} from '@inertiajs/react';
+import { BreadcrumbItem, } from '@/types';
+import { Head, useForm } from '@inertiajs/react';
 
 
 
@@ -11,11 +12,88 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: dashboard().url,
     },
 ];
-export default function edit() {
+export default function edit({ role, permissions, rolePermitssions }: any) {
+    interface FormData {
+        name: string;
+        permission_ids: number[];
+        guard_name: string;
+    }
+
+    const { data, setData, put, processing, errors } = useForm<FormData>({
+        name: role.name,
+        permission_ids: role.permissions.map((p: any) => p.id),
+        guard_name: role.guard_name,
+    });
+
+    const submit = (e: React.FormEvent) => {
+        e.preventDefault();
+        put(`/role/update/${role.id}`, {
+            forceFormData: true,
+        });
+    }
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Update Role" />
+            <div className='m-6'>
+                <form onSubmit={submit}>
+                    <div>
+                        <label className='block mb-2'>Role Name</label>
+                        <input type="text" value={data.name} onChange={(e) => setData('name', e.target.value)} className='border rounded px-3 py-2 w-full' />
+                        {errors.name && <div className='text-red-600 mt-2'>{errors.name}</div>}
+                    </div>
+                    <div className='mt-4'>
+                        <label className='block mb-2'>Guard Name</label>
+                        <select
+                            value={data.guard_name}
+                            onChange={(e) => setData('guard_name', e.target.value)}
+                            className='border rounded px-3 py-2 w-full mt-2'
+                        >
+                            <option selected value="web">web</option>
+                        </select>
+                        {errors.guard_name && <div className='text-red-600 mt-2'>{errors.guard_name}</div>}
+                    </div>
+                    <div className='mt-4'>
+                        <label className='block mb-2'>Permissions</label>
+                        {permissions.map((permission: any) => {
+                            const isChecked = data.permission_ids.includes(permission.id)
+
+                            return (
+                                <label
+                                    key={permission.id}
+                                    className="flex items-center gap-2 cursor-pointer"
+                                >
+                                    <input
+                                        type="checkbox"
+                                        value={permission.id}
+                                        checked={isChecked}
+                                        onChange={(e) => {
+                                            if (e.target.checked) {
+                                                setData('permission_ids', [...data.permission_ids, permission.id])
+                                            } else {
+                                                setData(
+                                                    'permission_ids',
+                                                    data.permission_ids.filter((id: number) => id !== permission.id)
+                                                )
+                                            }
+                                        }}
+                                        className="h-4 w-4"
+                                    />
+                                    <span className="text-sm">{permission.name}</span>
+                                    <span className="text-sm">{role.name}</span>
+                                </label>
+                            )
+                        })}
+                        {errors.permission_ids && <div className='text-red-600 mt-2'>{errors.permission_ids}</div>}
+                    </div>
+                    <div className='mt-6'>
+                        <Button type="submit" disabled={processing} >
+                            {processing ? 'Updating...' : 'Update Role'}
+                        </Button>
+                    </div>
+                </form>
+
+            </div>
         </AppLayout>
     );
 }
