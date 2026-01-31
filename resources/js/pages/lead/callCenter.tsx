55be1ccd7871d625c745272d884e75a7984b2b1a
Nowshad <lewsedmond@gmail.com>
2026-01-31 20:13:26 +0600

    add

--- resources/js/pages/lead/callCenter.tsx
+++ resources/js/pages/lead/callCenter.tsx
@@ -20,37 +20,38 @@ export default function Callcenter({ leads }: { leads: Lead[] }) {
     const [page, setPage] = useState(1);
     const [leadData, setLeadData] = useState<Lead[]>(leads);
 
-    useEffect(() => {
-        fetch(`/leads/call-center?page=${page}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setLeadData(data.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching leads:', error);
-            });
-    }, [page]);
-
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Lead Dashboard" />
+            <Button
+                disabled={!leads.links.find(l => l.label.includes('Next'))?.url}
+                onClick={() =>
+                    router.get(
+                        leads.links.find(l => l.label.includes('Next')).url,
+                        {},
+                        { preserveState: true, replace: true }
+                    )
+                }
+            >
+                Next
+            </Button>
             <div className='w-[100%] flex items-center justify-between'>
-                    <div className='grey-box w-[30%] h-[500px] flex flex-col'>
-                        <div className="commum border-1 border-b-cyan-200 w-full m-1 h-full">
-                            <div className='cart'>
-                                <Button onClick={ ()=>setPage(page+1)}>next</Button>
-                                {leadData.data.map((lead) => (
-                                    <div key={lead.id} className="commum  m-5 border-1 border-b-cyan-200 w-full m-1 h-full flex flex-col items-center justify-center">
-                                        <p>{lead.id}</p>
-                                        <h3>{lead.name}</h3>
-                                        <p>{lead.email}</p>
-                                        <p>{lead.phone}</p>
-                                    </div>
-                                ))}
-                            </div>
+                <div className='grey-box w-[30%] h-[500px] flex flex-col'>
+                    <div className="commum border-1 border-b-cyan-200 w-full m-1 h-full">
+                        <div className='cart'>
+                            <Button onClick={() => setPage(page + 1)}>next</Button>
+                            {leadData.data.map((lead) => (
+                                <div key={lead.id} className="commum  m-5 border-1 border-b-cyan-200 w-full m-1 h-full flex flex-col items-center justify-center">
+                                    <p>{lead.id}</p>
+                                    <h3>{lead.name}</h3>
+                                    <p>{lead.email}</p>
+                                    <p>{lead.phone}</p>
+                                </div>
+                            ))}
                         </div>
-                        
                     </div>
+
+                </div>
             </div>
         </AppLayout>
     );
